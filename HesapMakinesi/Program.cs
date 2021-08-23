a856065b74504746eea69048281d3cc6e1b204af
USER <USER@LAPTOP-UO7CH90K>
2021-08-23 17:07:48 +0300

    Add project files.

---
+++ HesapMakinesi/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HesapMakinesi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1;
+            int sayi2;
+            string islem;
+            double sonuc;
+
+
+            Console.WriteLine("Birinci sayıyı giriniz : ");
+            sayi1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("İkinci sayıyı giriniz : ");
+            sayi2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Yapmak istediğiniz işlemi seçiniz (+,-,*,/) : ");
+            islem = Console.ReadLine();
+
+            if (islem == "+")
+            {
+                sonuc = sayi1 + sayi2;
+                Console.WriteLine("sonuç : " + sonuc);
+
+            }
+
+            if (islem == "-")
+            {
+                sonuc = sayi1 - sayi2;
+                Console.WriteLine("sonuç : " + sonuc);
+            }
+
+            if (islem == "*")
+            {
+                sonuc = sayi1 * sayi2;
+                Console.WriteLine("sonuç : " + sonuc );
+
+            }
+
+            if (islem == "/")
+            {
+                sonuc = sayi1 / sayi2;
+                Console.WriteLine("sonuç : " + sonuc );
+
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
